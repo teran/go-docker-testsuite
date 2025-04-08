1d00a01cd458f71881127ba441bb038b4c3dac7d
Igor Shishkin <me@teran.dev>
2025-04-08 04:08:47 +0300

    Fix env tests by using t.Setenv instead of global one
    Signed-off-by: Igor Shishkin <me@teran.dev>

--- node_test.go
+++ node_test.go
@@ -45,13 +45,13 @@ func TestDockerIP(t *testing.T) {
 	r.Equal("127.0.0.1", ip)
 
 	// Valid DOCKER_HOST value for remote instance
-	_ = os.Setenv("DOCKER_HOST", "tcp://1.1.1.1:2376")
+	t.Setenv("DOCKER_HOST", "tcp://1.1.1.1:2376")
 	ip, err = DockerIP()
 	r.NoError(err)
 	r.Equal("1.1.1.1", ip)
 
 	// Invalid DOCKER_HOST value
-	_ = os.Setenv("DOCKER_HOST", "1.1.1.1")
+	t.Setenv("DOCKER_HOST", "1.1.1.1")
 	_, err = DockerIP()
 	r.Error(err)
 	r.Equal("malformed DOCKER_HOST value: empty host or port value", err.Error())
