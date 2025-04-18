baa3d7f7b5786bdd064ac42906b5db2fcfc3fd36
Igor Shishkin <me@teran.dev>
2025-04-18 07:04:58 +0300

    Add scylla 2025.1.1 & 6.2.3
    Signed-off-by: Igor Shishkin <me@teran.dev>

---
+++ applications/scylladb/versions/2025.1.1/version_test.go
@@ -0,0 +1,21 @@
+//go:build scylla
+
+package scylladb
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/teran/go-docker-testsuite/applications/scylladb/versions"
+)
+
+const image = "index.docker.io/scylladb/scylla:2025.1.1"
+
+func TestScyllaDBVersion(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), versions.ScyllaDBTestDefaultTimeout)
+	defer cancel()
+
+	suite.Run(t, versions.New(ctx, image))
+}
