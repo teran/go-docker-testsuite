74412954b0159699ac237b9505b4d0ecf528024e
Igor Shishkin <me@teran.dev>
2025-04-18 07:03:19 +0300

    Replace scylladb 6.1.0-rc1 w/ 6.1.5
    Signed-off-by: Igor Shishkin <me@teran.dev>

--- applications/scylladb/versions/6.1.5/version_test.go
+++ applications/scylladb/versions/6.1.5/version_test.go
@@ -9,14 +9,13 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	"github.com/teran/go-docker-testsuite/applications/scylladb/versions"
-	"github.com/teran/go-docker-testsuite/images"
 )
 
-const image = "index.docker.io/scylladb/scylla:6.1.0-rc1"
+const image = "index.docker.io/scylladb/scylla:6.1.5"
 
 func TestScyllaDBVersion(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), versions.ScyllaDBTestDefaultTimeout)
 	defer cancel()
 
-	suite.Run(t, versions.New(ctx, image)
+	suite.Run(t, versions.New(ctx, image))
 }
