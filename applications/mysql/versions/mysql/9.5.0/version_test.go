879a40c7c83b7073f4cd3c8c34bb3ddae1cd6f5a
Igor Shishkin <me@teran.dev>
2025-10-30 00:48:06 +0300

    Add mariadb 12.0.2 and mysql 9.5.0 (#144)
    Signed-off-by: Igor Shishkin <me@teran.dev>

---
+++ applications/mysql/versions/mysql/9.5.0/version_test.go
@@ -0,0 +1,19 @@
+package versions
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/suite"
+	"github.com/teran/go-docker-testsuite/applications/mysql/versions"
+)
+
+const image = "index.docker.io/library/mysql:9.5.0"
+
+func TestMySQL(t *testing.T) {
+	ctx, cancel := context.WithTimeout(t.Context(), 60*time.Second)
+	defer cancel()
+
+	suite.Run(t, versions.New(ctx, image))
+}
