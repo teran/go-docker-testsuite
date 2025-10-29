6796725fae083aa6b26efd7e07fdedbe50fe7f09
Igor Shishkin <me@teran.dev>
2025-10-30 00:01:42 +0300

    Add PostgreSQL 13.22, 14.19, 15.14, 16.10, 17.6 (#143)
    Signed-off-by: Igor Shishkin <me@teran.dev>

---
+++ applications/postgres/versions/16.10/version_test.go
@@ -0,0 +1,20 @@
+package postgres
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/teran/go-docker-testsuite/applications/postgres/versions"
+)
+
+const image = "index.docker.io/library/postgres:16.10"
+
+func TestPostgreSQLVersion(t *testing.T) {
+	ctx, cancel := context.WithTimeout(t.Context(), 3*time.Minute)
+	defer cancel()
+
+	suite.Run(t, versions.New(ctx, image))
+}
