bdee677514456d791d6caa235f8e81116d17dcc5
Igor Shishkin <me@teran.dev>
2026-06-25 12:44:07 +0300

    Add integration tests for Kafka versions 4.1.2, 4.2.1, 4.3.1 (#175)

---
+++ applications/kafka/versions/4.2.1/kafka_test.go
@@ -0,0 +1,35 @@
+//go:build integration
+
+package kafka
+
+import (
+    "context"
+    "net"
+    "strings"
+    "testing"
+    "time"
+
+    "github.com/teran/go-docker-testsuite/applications/kafka"
+)
+
+func TestKafkaVersion421(t *testing.T) {
+    ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+    defer cancel()
+
+    k, err := kafka.NewWithImage(ctx, "index.docker.io/apache/kafka:4.2.1")
+    if err != nil {
+        t.Fatalf("failed to create kafka container: %v", err)
+    }
+    defer func() { _ = k.Close(context.Background()) }()
+
+    url, err := k.GetBrokerURL(ctx)
+    if err != nil {
+        t.Fatalf("GetBrokerURL error: %v", err)
+    }
+    addr := strings.TrimPrefix(url, "tcp://")
+    conn, err := net.DialTimeout("tcp", addr, 5*time.Second)
+    if err != nil {
+        t.Fatalf("unable to connect to broker at %s: %v", addr, err)
+    }
+    _ = conn.Close()
+}
